35f4deeabbd0bf5627a0af91284e7710c2ea1992
Hailin Liu <0123liuhailin@gmail.com>
2024-03-02 23:30:19 +0300

    branch:cowsay homework 03 part 3 From [02 MAR 2024], Hailin Liu

--- 03_MergeRequirements/gameplay_io.py
+++ 03_MergeRequirements/gameplay_io.py
@@ -1,6 +1,11 @@
+import cowsay as cs
+import random
+
 def ask(prompt: str, valid: list[str] = None) -> str:
-    word = input(prompt)
+    cowname = random.choices(cs.list_cows())[0]
+    word = input(cs.cowsay(prompt, cow=cowname))
     return word
 
 def inform(format_string: str, bulls: int, cows: int) -> None:
-    print(format_string.format(bulls, cows))
+    cowname = random.choices(cs.list_cows())[0]
+    print(cs.cowsay(format_string.format(bulls, cows), cow=cowname))
